462325478c1b4408e366ea12b454903bdac9e7a6
Tigran Tch <nargitinthenight@gmail.com>
2017-04-10 23:54:18 +0200

    fix step-1

--- bin/run-j9.sh
+++ bin/run-j9.sh
@@ -2,5 +2,6 @@
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home
 
 $JAVA_HOME/bin/java \
-    --module-path dependencies:target/main/artifact \
+    --module-path target/main/artifact \
+    --add-modules ALL-DEFAULT \
     -m cart/com.swissquote.cart.Main
